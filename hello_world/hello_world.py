d9e15da4dc31726922c2e6517fb06f8b78c91d47
genis_castillo <genis.castillo@estudiantat.upc.edu>
2024-03-31 23:50:42 +0200

    Añadido gitignore y hechos dos primeros tutoriales

---
+++ hello_world/hello_world.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget
+
+
+# create the QApplication
+app = QApplication(sys.argv)
+
+# create the main window
+window = QWidget(windowTitle='Hello World')
+window.show()
+
+# start the event loop
+sys.exit(app.exec())
