74203ae6173e2c1a5ee090ef4ef8f03eab5d8037
genis_castillo <genis.castillo@estudiantat.upc.edu>
2024-04-01 12:05:37 +0200

    QHBox tutorial done

---
+++ Layout_management/QHBoxLayout/align_center.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout
+
+
+class MainWindow(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setWindowTitle('PyQt QHBoxLayout')
+
+        # create a layout
+        layout = QHBoxLayout()
+        self.setLayout(layout)
+
+        # add a spacer
+        layout.addStretch()
+
+        # create buttons and add them to the layout
+        titles = ['Yes', 'No', 'Cancel']
+        buttons = [QPushButton(title) for title in titles]
+        for button in buttons:
+            layout.addWidget(button)
+
+        # add a spacer
+        layout.addStretch()
+
+        # show the window
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec())
---
+++ Layout_management/QHBoxLayout/basic_QHBox.py
@@ -0,0 +1,28 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout
+
+
+class MainWindow(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setWindowTitle('PyQt QHBoxLayout')
+
+        # create a layout
+        layout = QHBoxLayout()
+        self.setLayout(layout)
+
+        # create buttons and add them to the layout
+        titles = ['Yes', 'No', 'Cancel', 'None of the others']
+        buttons = [QPushButton(title) for title in titles]
+        for button in buttons:
+            layout.addWidget(button)
+
+        # show the window
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec())
